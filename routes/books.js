c4bd16a46e44754f9d6006f0a73bfa4f3b479c8b
bitnami <bitnami@ubuntu.defaultdomain>
2017-03-03 19:22:36 +0000

    Added REST Support to provide a list of books and individual book records

---
+++ routes/books.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var books = [{
+		id: 1,
+		title: 'Book 1',
+		bookType: 'Hard Cover',
+		price: 9.50,
+		onHand: 20,
+		display: 7,
+		orderPoint: 10,
+		orderQuantity: 20,
+		lastUpdated: '2017-01-29T00:00:00.000Z'
+	}, {
+		id: 2,
+		title: 'Book 2',
+		bookType: 'Hard Cover',
+		price: 9.50,
+		onHand: 20,
+		display: 7,
+		orderPoint: 10,
+		orderQuantity: 20,
+		lastUpdated: '2017-01-30T00:00:00.000Z'
+	}
+];
+
+function bookByID(element) {
+		return element.id == this.id
+}
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+	res.send(JSON.stringify(books, 0, 2));
+});
+
+router.post('/', function (req, res, next) {
+	res.send("New Book Created Successfully");
+});
+
+router.get('/:bookID', function (req, res, next) {
+	// res.send("Book ID is: " + req.params.bookID);
+	res.send(JSON.stringify(books.find(bookByID,{id: req.params.bookID}), 0, 2));
+});
+
+module.exports = router;
