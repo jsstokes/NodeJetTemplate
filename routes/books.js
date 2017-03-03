30bf74090d469a00384113c3472fa1adc76787b0
bitnami <bitnami@ubuntu.defaultdomain>
2017-03-03 19:39:29 +0000

    Additional Book Reocrds

--- routes/books.js
+++ routes/books.js
@@ -20,7 +20,107 @@ var books = [{
 		orderPoint: 10,
 		orderQuantity: 20,
 		lastUpdated: '2017-01-30T00:00:00.000Z'
-	}
+	}, {
+		id: 2,
+		title: 'Book 2',
+		bookType: 'Hard Cover',
+		price: 9.50,
+		onHand: 20,
+		display: 7,
+		orderPoint: 10,
+		orderQuantity: 20,
+		lastUpdated: '2017-01-30T00:00:00.000Z'
+	}, {
+		id: 3,
+		title: 'Book 3',
+		bookType: 'Hard Cover',
+		price: 9.50,
+		onHand: 20,
+		display: 7,
+		orderPoint: 10,
+		orderQuantity: 20,
+		lastUpdated: '2017-01-30T00:00:00.000Z'
+	}, {
+		id: 4,
+		title: 'Book 4',
+		bookType: 'Hard Cover',
+		price: 9.50,
+		onHand: 20,
+		display: 7,
+		orderPoint: 10,
+		orderQuantity: 20,
+		lastUpdated: '2017-01-30T00:00:00.000Z'
+	}, {
+		id: 5,
+		title: 'Book 5',
+		bookType: 'Hard Cover',
+		price: 9.50,
+		onHand: 20,
+		display: 7,
+		orderPoint: 10,
+		orderQuantity: 20,
+		lastUpdated: '2017-01-30T00:00:00.000Z'
+	}, {
+		id: 6,
+		title: 'Book 6',
+		bookType: 'Hard Cover',
+		price: 9.50,
+		onHand: 20,
+		display: 7,
+		orderPoint: 10,
+		orderQuantity: 20,
+		lastUpdated: '2017-01-30T00:00:00.000Z'
+	}, {
+		id: 7,
+		title: 'Book 7',
+		bookType: 'Hard Cover',
+		price: 9.50,
+		onHand: 20,
+		display: 7,
+		orderPoint: 10,
+		orderQuantity: 20,
+		lastUpdated: '2017-01-30T00:00:00.000Z'
+	}, {
+		id: 8,
+		title: 'Book 8',
+		bookType: 'Hard Cover',
+		price: 9.50,
+		onHand: 20,
+		display: 7,
+		orderPoint: 10,
+		orderQuantity: 20,
+		lastUpdated: '2017-01-30T00:00:00.000Z'
+	}, {
+		id: 9,
+		title: 'Book 9',
+		bookType: 'Hard Cover',
+		price: 9.50,
+		onHand: 20,
+		display: 7,
+		orderPoint: 10,
+		orderQuantity: 20,
+		lastUpdated: '2017-01-30T00:00:00.000Z'
+	}, {
+		id: 10,
+		title: 'Book 10',
+		bookType: 'Hard Cover',
+		price: 9.50,
+		onHand: 20,
+		display: 7,
+		orderPoint: 10,
+		orderQuantity: 20,
+		lastUpdated: '2017-01-30T00:00:00.000Z'
+	}, {
+		id: 11,
+		title: 'Book 11',
+		bookType: 'Hard Cover',
+		price: 9.50,
+		onHand: 20,
+		display: 7,
+		orderPoint: 10,
+		orderQuantity: 20,
+		lastUpdated: '2017-01-30T00:00:00.000Z'
+	}, 
 ];
 
 function bookByID(element) {
